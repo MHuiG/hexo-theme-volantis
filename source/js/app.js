10f30f3657d6e873fca3bbc22afaf28d3fc68dec
MHuiG <616001163@qq.com>
2020-06-30 20:29:52 +0800

    fix

--- source/js/app.js
+++ source/js/app.js
@@ -448,8 +448,8 @@ setInterval("LogoChange()",1000);
 if(window.location.hash){
 	var checkExist = setInterval(function() {
 	   if (typeof jQuery == 'undefined'){return;}
-	   if ($("#"+decodeURI(window.location.hash.split("#")[1])).length) {
-		  $('html, body').animate({scrollTop: $("#"+decodeURI(window.location.hash.split("#")[1])).offset().top-90}, 1000);
+	   if ($("#"+decodeURI(window.location.hash.split("#")[1]).replace(/\ /g,"-")).length) {
+		  $('html, body').animate({scrollTop: $("#"+decodeURI(window.location.hash.split("#")[1]).replace(/\ /g,"-")).offset().top-10}, 500);
 		  clearInterval(checkExist);
 	   }
 	}, 100);
